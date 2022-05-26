fa909497afb73780896026f66859075aef47ff70
da0hn <gabrielhondacba@gmail.com>
2022-05-26 13:09:52 -0400

    feat: make `Pokemon` implements `compareTo` interface

--- src/main/java/br/com/gabriel/looqbox/challenge/core/Pokemon.java
+++ src/main/java/br/com/gabriel/looqbox/challenge/core/Pokemon.java
@@ -2,5 +2,9 @@ package br.com.gabriel.looqbox.challenge.core;
 
 public record Pokemon(
   String name
-) {
+) implements Comparable<Pokemon> {
+  @Override public int compareTo(final Pokemon other) {
+    if(other == null) return 1;
+    return this.name.compareTo(other.name);
+  }
 }
